ca1c625b225d615f19e3f3f2eb43ac7b3f0e9ac7
Dokisankon <hlyang.cn@gmail.com>
2024-12-08 14:35:28 +0000

    Upload the code

---
+++ Question2.py
@@ -0,0 +1,107 @@
+import numpy as np
+import pandas as pd
+import math
+from scipy.stats import *
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+## (i)(a)
+mean = 2.6
+x = 2
+
+prob = (mean**x * np.exp(-mean))/(math.factorial(x))
+
+print(f"The probability of having {x} defect in 1 meter is {prob}")
+
+## (i)(b)
+def poisson_prob(mean, x):
+    return(mean**x * np.exp(-mean))/(math.factorial(x))
+
+prob = 1
+for x in range(9):
+    prob -= poisson_prob(mean = 2.6*5, x = x)
+    
+print(f"The probability of having more than 8 defect in 5 meter is {prob}")
+
+##(i)(c)
+prob = 0
+for x in range(20):
+    prob += poisson_prob(mean = 2.6*10, x = x)
+    
+print(f"The probability of having less than 20 defect in 10 meter is {prob}")
+
+
+##(ii)(a)
+data = np.array([33.75, 33.05, 34.00, 33.81, 33.46, 34.02, 33.58, 33.27, 33.49, 33.20, 34.62, 33.00, 33.54, 34.12, 33.84])
+
+print(data.mean())
+
+##(ii)(b)
+
+diff_norm = norm.ppf(0.975)
+std = np.power(data.var(), 0.5)
+
+diff_range = diff_norm * (std / np.sqrt(15))
+print(data.mean() + diff_range)
+print(data.mean() - diff_range)
+
+##(ii)(c)
+mean = np.mean(data)
+std = np.std(data)
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+
+num_bins = int(1 + 3.322 * np.log10(len(data)))
+bins = np.linspace(min(data), max(data), num_bins + 1)  # 等间隔区间
+
+# ax = sns.histplot(data, bins = bins)
+# plt.title('Historgram of viscosity')
+# plt.show()
+
+hist, bin_edges = np.histogram(data, bins = num_bins)
+
+modify_bins = bins
+modify_bins[0] = 0
+modify_bins[-1]= 99999
+expected_probs = norm.cdf(modify_bins, mean, std)
+expected_probs = expected_probs[1:] - expected_probs[:-1]
+
+expected = np.array(expected_probs) * len(data)
+
+chi2_stat, p_value = chisquare(f_obs=hist, f_exp=expected)
+print(f"Chi-value: {chi2_stat}")
+print(f"p-value: {p_value}")
+print(mean)
+print(std)
+
+temp =  norm.cdf([33,35], mean, std)
+print(temp[0] + (1 - temp[1]))
+
+# exit()
+hist, bin_edges = np.histogram(data, bins = bins)
+cdf_values = norm.cdf(bin_edges, data.mean(), std)
+# print(pd.DataFrame({'cdf':cdf_values[1:],'freq':hist}))
+# print(bin_edges)
+
+hist_df = pd.DataFrame({"Length": bin_edges[:-1],
+                        "Frequency": hist})
+bin_frequency = []
+for x in range(len(cdf_values)-1):
+    if x == 0:
+        frequency = cdf_values[0]
+    else:
+        frequency = cdf_values[x] - cdf_values[x-1]
+    bin_frequency.append(frequency)
+bin_frequency = np.array(bin_frequency)
+expected_values = bin_frequency * len(data)
+
+print(hist)
+print(expected_values)
+
+chi_square_values = (hist - expected_values)**2 /expected_values
+chi_square_df = pd.DataFrame({'x^2': chi_square_values})
+chi_square_df.loc['sum'] = chi_square_df.sum()
+print(chi_square_df)
+
+print(chi())
---
+++ Question3.py
@@ -0,0 +1,144 @@
+import numpy as np
+import pandas as pd
+import math
+from scipy.stats import *
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+data_1 = {
+    "Disc #1": [55, 72, 26, 65, 104, 62, 89, 52, 76, 46, 100, 72, 35]}
+data_2 = {
+    "Disc #2": [71, 62, 37, 46, 36,  44, 63, 71, 49, 44, 41,  76, 55, 64, 41]
+}
+df_1 = pd.DataFrame(data_1)
+df_2 = pd.DataFrame(data_2)
+
+print(df_1.describe())
+print(df_2.describe())
+
+## F-test
+Var_1 = df_1.var()[0]
+Var_2 = df_2.var()[0]
+
+if Var_1 > Var_2:
+    F = Var_1 / Var_2
+    dfn, dfd = len(df_1) - 1, len(df_2) - 1  # 自由度
+else: 
+    F = Var_2 / Var_1
+    dfn, dfd = len(df_2) - 1, len(df_1) - 1
+    
+p_value = 1 - f.cdf(F, dfn, dfd)
+
+print('=========F-test=========')
+print(f"Variance of Disc #1: {Var_1:.4f}")
+print(f"Variance of Disc #2: {Var_2:.4f}")
+print(f"F_value: {F:.4f}")
+print(f"p_value: {p_value:.4f}")
+
+
+# 判断是否拒绝原假设
+alpha = 0.05
+if p_value < alpha:
+    print("Rejct null hypothesis: Two data have different Variance.")
+else:
+    print("Fail to rejct null hypothesis: Two data have same Variance.")
+    
+
+print("\n")
+print("=========T-test=========")
+T, p_value = ttest_ind(df_1['Disc #1'].to_list(), df_2['Disc #2'].to_list(), equal_var=False)
+print(f"T_value: {T:.4f}")
+print(f"p_value: {p_value:.4f}")
+
+alpha = 0.05
+if p_value < alpha:
+    print("Rejct null hypothesis: Two data have different Mean value.")
+else:
+    print("Fail to rejct null hypothesis: Two data have same Mean value.")
+    
+print("\n")
+
+## Outlier check
+data1 = [55, 72, 26, 65, 104, 62, 89, 52, 76, 46, 100, 72, 35]
+data2 = [71, 62, 37, 46, 36, 44, 63, 71, 49, 44, 41, 76, 55, 64, 41]
+
+
+def detect_outliers_iqr(data):
+    Q1 = np.percentile(data, 25)  # 第 1 四分位数
+    Q3 = np.percentile(data, 75)  # 第 3 四分位数
+    IQR = Q3 - Q1  # 四分位距
+    lower_bound = Q1 - 1.5 * IQR  # 下边界
+    upper_bound = Q3 + 1.5 * IQR  # 上边界
+    outliers = [x for x in data if x < lower_bound or x > upper_bound]
+    return outliers, lower_bound, upper_bound
+
+# 检测两组数据的异常值
+outliers1, lb1, ub1 = detect_outliers_iqr(data1)
+outliers2, lb2, ub2 = detect_outliers_iqr(data2)
+
+print("Outliers analisys")
+print("Disc #1 Outliers:", outliers1)
+print(f"Disc #1 range: [{lb1:.2f}, {ub1:.2f}]")
+print("Disc #2 Outliers:", outliers2)
+print(f"Disc #2 range: [{lb2:.2f}, {ub2:.2f}]")
+print("\n")
+
+## (ii)
+
+data = {
+    "Month": [
+        "2022-01-01", "2022-02-01", "2022-03-01", "2022-04-01", "2022-05-01",
+        "2022-06-01", "2022-07-01", "2022-08-01", "2022-09-01", "2022-10-01",
+        "2022-11-01", "2022-12-01", "2023-01-01", "2023-02-01", "2023-03-01",
+        "2023-04-01", "2023-05-01", "2023-06-01", "2023-07-01", "2023-08-01",
+        "2023-09-01", "2023-10-01", "2023-11-01", "2023-12-01"
+    ],
+    "Social Media Engagement": [
+        4174, 4507, 1860, 2294, 2130, 2095, 4772, 4092, 2638, 3169, 
+        1466, 2238, 1330, 2482, 3135, 4444, 4171, 3919, 4735, 1130, 
+        2685, 4380, 1769, 3391
+    ],
+    "E-commerce Sales (GBP)": [
+        43733.69, 40867.71, 18153.32, 21248.07, 18954.22, 21034.44, 45208.41, 
+        40848.7, 26005.26, 30055.2, 13297.71, 21862.1, 13369.99, 24975.03, 
+        31345.29, 45607.35, 44238.92, 37214.39, 48760.05, 12248.26, 
+        28668.32, 46515.17, 17518.32, 35302.87
+    ]
+}
+
+# Load into a DataFrame
+df = pd.DataFrame(data)
+
+print(df)
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax = plt.plot(df['Social Media Engagement'],df['E-commerce Sales (GBP)'],'o')
+# plt.xlabel('Social Media Engagement')
+# plt.ylabel('E-commerce Sales (GBP)')
+# plt.title('Engagement v.s. Sales')
+# plt.show()
+
+## correlation test pearson's r
+SME = df['Social Media Engagement']
+Sales = df['E-commerce Sales (GBP)']
+
+corr, p_value = pearsonr(SME, Sales)
+
+
+print()
+print(f'The pearson correlation value is: {corr:.4f}')
+print(f'The P-value of this test is: {p_value:.4f}')
+print()
+
+import statsmodels.api as sm
+
+X = sm.add_constant(SME.tolist())
+y = Sales.tolist()
+
+model = sm.OLS(y, X).fit()
+
+print(model.summary())
+print()
